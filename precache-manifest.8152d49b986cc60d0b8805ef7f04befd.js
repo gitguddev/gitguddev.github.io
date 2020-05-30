0d74cfeb5992d90f58e7b46025f72601230085ef
Thanawat Yodnil <littleboycoding@gmail.com>
2020-05-31 04:52:10 +0700

    Updates

---
+++ precache-manifest.8152d49b986cc60d0b8805ef7f04befd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b11d0d0330c3c1ae0445eb947c771e4",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "15d31c0fd0f1bfdb6511",
+    "url": "/website/static/css/main.d61d939f.chunk.css"
+  },
+  {
+    "revision": "24ccba40be600dd95a9f",
+    "url": "/website/static/js/2.1b459a4c.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/website/static/js/2.1b459a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d31c0fd0f1bfdb6511",
+    "url": "/website/static/js/main.6ec63db6.chunk.js"
+  },
+  {
+    "revision": "b56fd8800aeaade8aa1a",
+    "url": "/website/static/js/runtime-main.1c8d7277.js"
+  }
+]);
