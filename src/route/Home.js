d7df0a8aa7bd30081e4338ae1fcbfd4d2b355f14
Thanawat Yodnil <littleboycoding@gmail.com>
2020-09-15 21:30:13 +0700

    Fixed minor bug, Tweak member structure, Thanawat styling tweaked

--- src/route/Home.js
+++ src/route/Home.js
@@ -1,65 +1,11 @@
 import style from "../css/home.module.css";
 import React, { useState, useRef } from "react";
 
-import Thanawat from "../member/Thanawat";
-
 import album1 from "../album/album1.jpg";
 import album2 from "../album/album2.jpg";
 import album3 from "../album/album3.jpg";
 
-const userList = [
-  {
-    name: "Thanawat Yodnil",
-    user: "littleboycoding",
-    Component: Thanawat,
-    img:
-      "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/76612185_1751899918276565_7047093484993380352_n.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=lehHhRNYmBkAX-KDjCu&_nc_ht=scontent.fbkk5-3.fna&oh=6b1915b25d4cf4da62d54fef35724651&oe=5F80343D",
-  },
-  {
-    name: "Protae",
-    user: "taetargmo",
-    Component: null,
-    img:
-      "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/82859128_2582758038718349_5708485176787468288_n.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=Yn0Lq0MZP3QAX92E7WY&_nc_ht=scontent.fbkk5-3.fna&oh=1450f55969536b7f63ab0465d3c71c10&oe=5F81AAF1",
-  },
-  {
-    name: "Nanthiya Pitisaeng",
-    user: "aonann",
-    Component: null,
-    img:
-      "https://scontent.fbkk5-5.fna.fbcdn.net/v/t1.0-9/118777158_919280675261710_3009010750730749915_n.jpg?_nc_cat=104&_nc_sid=8bfeb9&_nc_ohc=4RRT7sWaY7gAX8mHZP9&_nc_ht=scontent.fbkk5-5.fna&oh=237c5736753058d9d0dddb7a871429c1&oe=5F8323E2",
-  },
-  {
-    name: "Jiranat Juntamarai",
-    user: "pupae",
-    Component: null,
-    img:
-      "https://scontent.fbkk5-5.fna.fbcdn.net/v/t1.0-9/90113335_1779067795561337_2267110568936603648_o.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=_3rZge6XBNkAX89z_H6&_nc_ht=scontent.fbkk5-5.fna&oh=f24e1883d2cdc25267c7fba69558eda9&oe=5F822771",
-  },
-  {
-    name: "Natthakiat Jaruenchai",
-    user: "UserXCodert",
-    Component: null,
-    img:
-      "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/116584317_606879683541543_3734934671500337110_o.jpg?_nc_cat=105&_nc_sid=174925&_nc_ohc=LchyKcRWOjQAX-JyEsa&_nc_ht=scontent.fbkk5-3.fna&oh=a359de28e87d6550774a15251eb7056d&oe=5F835C6F",
-  },
-  {
-    name: "North",
-    user: "north",
-    Component: null,
-    img:
-      "https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.0-9/118791412_2695907100691047_313690294550642642_o.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=FCGkX4OCiAcAX9iudpD&_nc_ht=scontent.fbkk5-6.fna&oh=1cd9e5a990f5f85cdaa31ae50a7a66e0&oe=5F8150B4",
-  },
-  {
-    name: "Koheng Chokwiwat",
-    user: "khc",
-    Component: null,
-    img:
-      "https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/118578961_978147529324966_9050996599303733150_o.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=8HztZOxRh30AX8pBRdg&_nc_ht=scontent.fbkk5-4.fna&oh=e00652bd382599e5d199b7a21bc49cd4&oe=5F82F907",
-  },
-];
-
-console.log(userList)
+import MemberList from "../member/memberList";
 
 function Teletype() {
   const [currentCode, updateCode] = useState("");
@@ -107,7 +53,7 @@ function Header() {
         <span className={style.info}>Learn to be Developer</span>
 
         <div className={style.memberList}>
-          {userList.map((user) => {
+          {MemberList.map((user) => {
             return (
               <a href={"#" + user.user}>
                 <img className={style.memberLogo} src={user.img} />
@@ -180,56 +126,44 @@ function Home() {
       </div>
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white",
-          height: 150,
           fontSize: 26,
         }}
         className={style.parallax}
       >
         {"<สมาชิกทั้งหมด />"}
       </div>
-      <MemberList />
+      <Members />
     </div>
   );
 }
 
-function MemberList() {
-  return (
-    <>
-      {/* <MemberSection username="littleboycoding">
-        <Thanawat />
-      </MemberSection>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white",
-          height: 150,
-          fontSize: 26,
-        }}
-        className={style.parallax}
-      >
-        {"Experimenting !"}
-      </div>
-      <MemberSection username="taetargmo">
-        <Thanawat />
-      </MemberSection> */}
-      {userList.map(map => {
-        return <MemberSection username={map.user}>
-            {<map.Component /> && null}
-        </MemberSection>
-      })}
-    </>
+function Members() {
+  return MemberList.map(
+    (map, index) =>
+      map.Component && (
+        <>
+          {index > 0 && (
+            <div
+              className={style.parallax}
+            >
+              {/* {(index + 1) + ". " + map.name} */}
+            </div>
+          )}
+          <MemberSection username={map.user}>
+            <map.Component />
+          </MemberSection>
+        </>
+      )
   );
 }
 
 function MemberSection(props) {
   return (
-    <div id={props.username} className={style.section} style={{ backgroundColor: "white" }}>
+    <div
+      id={props.username}
+      className={style.section}
+      style={{ backgroundColor: "white" }}
+    >
       {props.children}
     </div>
   );
